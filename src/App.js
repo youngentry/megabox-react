41551a76dfbf6e73b92dee41d982307f147cf025
youngentry <gentry_@naver.com>
2022-10-04 14:00:48 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,21 +20,15 @@ function App() {
         const key = "b4f3293a5bbeeb23428ff7f42088c8f2";
         const url = `http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${key}&targetDt=20120101`;
         const getMovie = async () => {
-            const res = await axios
-                .get(url)
-                .then((res) =>
-                    res.data.boxOfficeResult.dailyBoxOfficeList.map((it) => {
-                        return {
-                            rank: it.rank,
-                            movieNm: it.movieNm,
-                            openDt: it.openDt,
-                        };
-                    })
-                )
-                .then((res) => {
-                    console.log("async:", res);
-                    setItm(res.data.boxOfficeResult.dailyBoxOfficeList);
-                });
+            const sdsd = await axios.get(url);
+            sdsd.data.boxOfficeResult.dailyBoxOfficeList.map((it) => {
+                return {
+                    rank: it.rank,
+                    movieNm: it.movieNm,
+                    openDt: it.openDt,
+                };
+            });
+            console.log(sdsd.data.boxOfficeResult);
             // console.log(res.data.boxOfficeResult.dailyBoxOfficeList);
             // const movieData = res.data.boxOfficeResult.dailyBoxOfficeList.map(
             //     (it) => {
@@ -51,22 +45,7 @@ function App() {
         getMovie();
     }, []);
 
-    return (
-        <Wrapper>
-            {console.log("App.js inner:", itm)}
-            <Header />
-            <Routes>
-                <Route path="/" element={<Main />} />
-                <Route path="/theater" element={<Theater />} />
-                <Route path="/event/*" element={<Event />} />
-                <Route path="/store/*" element={<Store />} />
-                <Route path="/benefit/*" element={<Benefit />} />
-                <Route path="/movies/*" element={<Movies movie={itm} />} />
-                <Route path="/ticketing" element={<Ticketing />} />
-            </Routes>
-            <Footer />
-        </Wrapper>
-    );
+    return <>ddd</>;
 }
 
 export default App;
