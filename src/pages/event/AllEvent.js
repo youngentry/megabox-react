d8b4d6d36fe6cdf9723d843d1728d22108249a99
youngentry <gentry_@naver.com>
2022-10-12 17:39:04 +0900

    이벤트 전체에 영화,극장,제휴,시사회 나타나게하기
    다음으로 메인페이지 영화 나타나게 하기

--- src/pages/event/AllEvent.js
+++ src/pages/event/AllEvent.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { FaAngleRight } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import Curtaincall from "./Curtaincall";
 import EventMovie from "./EventMovie";
 import Megapick from "./Megapick";
+import Promotion from "./Promotion";
 import Recommendation from "./Recommendation";
+import Theater from "./Theater";
 
 const AllEvent = ({ MEGAPICKDATA }) => {
     return (
@@ -21,13 +24,40 @@ const AllEvent = ({ MEGAPICKDATA }) => {
                         <Megapick MEGAPICKDATA={MEGAPICKDATA} num={4} />
                     </li>
                     <li>
-                        <div className="title megapick">
+                        <div className="title eventmovie">
                             <h3>영화</h3>
                             <Link className="more" to="/event/eventmovie">
                                 <span>더보기</span> <FaAngleRight />
                             </Link>
                         </div>
                         <EventMovie num={4} />
+                    </li>
+                    <li>
+                        <div className="title theater">
+                            <h3>극장</h3>
+                            <Link className="more" to="/event/theater">
+                                <span>더보기</span> <FaAngleRight />
+                            </Link>
+                        </div>
+                        <Theater num={4} />
+                    </li>
+                    <li>
+                        <div className="title promotion">
+                            <h3>제휴/할인</h3>
+                            <Link className="more" to="/event/promotion">
+                                <span>더보기</span> <FaAngleRight />
+                            </Link>
+                        </div>
+                        <Promotion num={4} />
+                    </li>
+                    <li>
+                        <div className="title curtaincall">
+                            <h3>시사회/무대인사</h3>
+                            <Link className="more" to="/event/curtaincall">
+                                <span>더보기</span> <FaAngleRight />
+                            </Link>
+                        </div>
+                        <Curtaincall num={4} />
                     </li>
                 </ul>
             </div>
