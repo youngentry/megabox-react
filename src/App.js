3b8900d18cc5805a8cf73e27d6e937dceda2c96e
youngentry <gentry_@naver.com>
2022-10-13 10:53:51 +0900

    섹션 컨텐츠 간격 margin-bottom 80px > 70px
    이벤트, 스토어, margin-bottom 80px > 70px혜택 간격 수정
    극장 margin-bottom 70px 추가

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Wrapper from "./pages/Wrapper";
 import Header from "./pages/main/Header";
 import Main from "./pages/main/Main";
-import Theater from "./pages/main/Theater";
+import Theater from "./pages/theater/Theater";
 import Event from "./pages/event/Event";
 import Ticketing from "./pages/main/Ticketing";
 import Footer from "./pages/main/Footer";
@@ -11,7 +11,6 @@ import Movies from "./pages/main/Movies";
 import "./css/basic.scss";
 import Store from "./pages/store/Store";
 import Benefit from "./pages/benefit/Benefit";
-import { useEffect, useRef, useState } from "react";
 import { BOXOFFICEDATA } from "./data/BOXOFFICEDATA";
 import { NAVERMOVIEDATA } from "./data/NAVERDATA";
 
