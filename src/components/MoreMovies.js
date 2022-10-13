13ebfe75918eaa44abad719e440a25d40f88e42b
youngentry <gentry_@naver.com>
2022-10-13 10:39:39 +0900

    221013 MoreMovies 더보기버튼 css수정
    더보기 버튼 css 수정하고 html 아이콘 추가함

--- src/components/MoreMovies.js
+++ src/components/MoreMovies.js
@@ -1,5 +1,6 @@
 import React, { useRef } from "react";
 import "../css/components/MoreMovies.scss";
+import { FaAngleDown } from "react-icons/fa";
 
 const MoreMovies = ({ setShowCount }) => {
     const num = useRef(1);
@@ -10,7 +11,20 @@ const MoreMovies = ({ setShowCount }) => {
     };
 
     // 4 * 4 까지 출력하게 되면 더보기 버튼이 사라짐
-    return <div className="MoreMovies">{num.current < 4 ? <button onClick={() => showCountPlus(num)}>더보기</button> : ""}</div>;
+    return (
+        <div className="MoreMovies">
+            {num.current < 4 ? (
+                <button onClick={() => showCountPlus(num)}>
+                    <span>더보기</span>
+                    <i>
+                        <FaAngleDown />
+                    </i>
+                </button>
+            ) : (
+                ""
+            )}
+        </div>
+    );
 };
 
 export default MoreMovies;
